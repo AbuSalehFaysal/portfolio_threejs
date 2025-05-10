8d6e91b8b0df995e7d8041599de832cdb5b1b7cf
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2025-05-10 03:26:46 -0230

    ring icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Navbar from "./sections/Navbar.jsx";
-import Hero from "./sections/Hero.jsx";
+import React from 'react';
+import Navbar from './sections/Navbar.jsx';
+import Hero from './sections/Hero.jsx';
 
 const App = () => {
-    return (
-        <main className="max-w-7xl mx-auto">
-            <Navbar />
-            <Hero />
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+export default App;
